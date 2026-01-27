61e5304fa8ef296ad9b797f136bd626d916ffeb7
krishna som <krishnarajput36890@gmail.com>
2026-01-27 15:50:52 +0530

    Updated

---
+++ Lab_0/03.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter the first Number : ");
+    scanf("%d",&x);
+    printf("Enter the second Number : ");
+    scanf("%d",&y);
+    printf("Sum of %d and %d is %d\n",x,y,x+y);
+    printf("Product of %d and %d is %d\n",x,y,x*y);
+    return 0;
+
+}
